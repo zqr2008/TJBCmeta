6dc64a8ff74bc5b888c93753dac51e48f096a169
zqr2008 <73268508+zqr2008@users.noreply.github.com>
2025-04-29 10:13:30 +0800

    Create singularity_envirment_step2.sh

---
+++ PanPhlAn/singularity_envirment_step2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/share/app/singularity/3.8.1/bin/singularity exec --cleanenv --bind /hwfssz1,/zfssz8,/jdfssz1,/hwfssz5 /hwfssz5/CNGB_RC/P23Z10200N0829/zhaiqiangrong/fecalmeta/pantrial/siffile/dockerhub.centos7.9.2009.development.basic.omicsbasic.meta_v2.1.sif bash /hwfssz5/CNGB_RC/P23Z10200N0829/zhaiqiangrong/fecalmeta/pantrial/Bacteroides_thetaiotaomicron_analysis/step2.sh
